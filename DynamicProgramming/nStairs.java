04a9c5c6b6089f47fd57936985e2da05eb91cedb
sunaina <sunainavirdi.5341@gmail.com>
2025-04-12 13:56:28 +0530

    updated

--- DynamicProgramming/nStairs.java
+++ DynamicProgramming/nStairs.java
@@ -1,5 +1,7 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class nStairs {
     // recursive solution - TLE
     public static long countDistinctWayToClimbStair(int nStairs) {
@@ -10,6 +12,23 @@ public class nStairs {
 		long right = countDistinctWayToClimbStair(nStairs-2);
 		return left + right;
 	}
+
+    // MEMOIZATION
+    public static long solve(int idx, long dp[]) {
+        if (idx == 0 || idx == 1) return 1;
+
+        if (dp[idx] != -1) return dp[idx];
+
+        long left = solve(idx - 1, dp);
+        long right = solve(idx - 2, dp);
+        return dp[idx] = left + right;
+    }
+
+    public static long countDistinctWayToClimbStair2(int nStairs) {
+        long[] dp = new long[nStairs + 1];
+        Arrays.fill(dp, -1); // ✅ Proper initialization
+        return solve(nStairs, dp);
+    }
     public static void main(String[] args) {
         int n = 3;
         System.out.print(countDistinctWayToClimbStair(n));
