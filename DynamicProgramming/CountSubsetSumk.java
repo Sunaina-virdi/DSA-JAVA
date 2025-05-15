ab4a573a17519ee8a84bba79e30335980e1f727e
sunaina <sunainavirdi.5341@gmail.com>
2025-05-15 23:03:58 +0530

    more dp ques

--- DynamicProgramming/CountSubsetSumk.java
+++ DynamicProgramming/CountSubsetSumk.java
@@ -1,12 +1,15 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class CountSubsetSumk {
     // recursion 
     // TC - (2^n)
     // SC - O(N)
-     public static int solve(int num[],int idx,int tar){
+    public static int solve(int num[],int idx,int tar){
         if(tar == 0) return 1;
-        if(idx < 0 || tar < 0) return 0;
+        if(idx == 0) return num[0] == tar ? 1 : 0;
+        // if(idx < 0 || tar < 0) return 0;
         
         int exclude = solve(num,idx-1,tar);
         int include = 0;
@@ -15,8 +18,31 @@ public class CountSubsetSumk {
         }
         return include + exclude;
     }
+    // TC - O(N * tar)
+    // SC - O(N * tar) + O(N)
+    public static int memo(int num[],int idx,int tar,int dp[][]){
+        // if(tar == 0) return 1;
+        if (idx == 0) {
+            if(tar == 0 && num[0] == 0) return 2;
+            if(tar == 0 || tar == num[0]) return 1;
+            return 0;
+        }
+        if(dp[idx][tar] != -1) return dp[idx][tar];
+        
+        int exclude = memo(num,idx-1,tar,dp);
+        int include = 0;
+        if(num[idx] <= tar){
+            include = memo(num,idx-1,tar-num[idx],dp);
+        }
+        return dp[idx][tar] = (include + exclude) % 1000000007;
+    }
     public static int findWays(int num[], int tar) {
-        return solve(num,num.length-1,tar);
+        int dp[][] = new int[num.length][tar+1];
+        for(int i[]: dp){
+            Arrays.fill(i,-1);
+        }
+        return memo(num,num.length - 1,tar,dp);
+        // return solve(num,num.length-1,tar);
     }
     public static void main(String[] args) {
         int num[] = {1,1,4,5};
---
+++ DynamicProgramming/PartitionWithDiffK.java
@@ -0,0 +1,43 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class PartitionWithDiffK {
+     static int mod = (int)(1e9 + 7);
+	public static int memo(int idx,int tar,int arr[],int dp[][]){
+		if(idx == 0){
+			if(tar == 0 && arr[0] == 0) return 2;
+			if(tar == 0 || tar == arr[0]) return 1;
+			return 0;
+		}
+		if(dp[idx][tar] != -1) return dp[idx][tar];
+		int skip = memo(idx-1,tar,arr,dp);
+		int take = 0;
+		if(arr[idx] <= tar){
+			take = memo(idx-1,tar-arr[idx],arr,dp);
+		}
+		return dp[idx][tar] = (skip + take)% mod;
+	}
+	public static int solve(int arr[],int tar){
+		int n = arr.length;
+		int dp[][] = new int[n][tar+1];
+		for(int i[]:dp){
+			Arrays.fill(i,-1);
+		}
+		return memo(n-1,tar,arr,dp);
+	}
+	public static int countPartitions(int[] arr,int d) {
+		// Write your code here.
+		int totalSum = 0;
+		for(int i : arr){
+			totalSum += i;
+		}
+		if(totalSum - d < 0 || (totalSum -d) % 2 != 0) return 0;
+		return solve(arr,(totalSum - d )/ 2);
+    }
+    public static void main(String[] args) {
+        int d = 0;
+        int arr[] = {1,1,1,1};
+        System.out.println(countPartitions(arr, d));
+    }
+}
